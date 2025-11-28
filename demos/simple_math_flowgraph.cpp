c124631a8926e8a270d1b730b1b0272958b337ba
Derek Kozel <dkozel@deepsig.ai>
2025-11-28 16:33:42 +0000

    More status checks, still no terminate

--- demos/simple_math_flowgraph.cpp
+++ demos/simple_math_flowgraph.cpp
@@ -1,7 +1,9 @@
 #include <fmt/core.h>
+
 #include <gnuradio-4.0/Graph.hpp>
 #include <gnuradio-4.0/Scheduler.hpp>
 
+// Blocks
 #include <gnuradio-4.0/basic/SignalGenerator.hpp>
 #include <gnuradio-4.0/math/Math.hpp>
 #include <gnuradio-4.0/testing/NullSources.hpp>
@@ -11,6 +13,7 @@ int main()
     float sample_rate = 32000;
 
     gr::Graph fg;
+
     auto& source = fg.emplaceBlock<gr::basic::SignalGenerator<float>>({
         {"sample_rate", sample_rate},
         {"frequency", 0.0f},
@@ -24,21 +27,35 @@ int main()
     });
 
     auto& head = fg.emplaceBlock<gr::testing::HeadBlock<float>>({
-        {"n_samples_max", 1000}
+        {"n_samples_max", 64000}
     });
 
     auto& sink = fg.emplaceBlock<gr::testing::NullSink<float>>();
 
-    [[maybe_unused]] auto conn1 = fg.connect<"out">(source).to<"in">(add);
-    [[maybe_unused]] auto conn2 = fg.connect<"out">(add).to<"in">(head);
-    [[maybe_unused]] auto conn3 = fg.connect<"out">(head).to<"in">(sink);
+    const char* connection_error = "connection_error";
+    if (fg.connect<"out">(source).to<"in">(add) != gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
+    }
+    if (fg.connect<"out">(add).to<"in">(head) != gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
+    }
+    if (fg.connect<"out">(head).to<"in">(sink) != gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
+    }
 
     fmt::print("Flowgraph created with {} blocks\n", fg.blocks().size());
     fmt::print("Running scheduler...\n");
 
-    gr::scheduler::Simple sched;
-    auto ret = sched.exchange(std::move(fg));
-    [[maybe_unused]] auto result = sched.runAndWait();
+    gr::scheduler::Simple<gr::scheduler::ExecutionPolicy::singleThreaded> sched;
+    if (auto ret = sched.exchange(std::move(fg)); !ret) {
+        throw std::runtime_error(std::format("failed to initialize scheduler: {}", ret.error()));
+    }
+
+    const auto ret = sched.runAndWait();
+    if (!ret.has_value()) {
+        std::print("scheduler error: {}", ret.error());
+        std::exit(1);
+    }
 
     fmt::print("Scheduler finished\n");
 
